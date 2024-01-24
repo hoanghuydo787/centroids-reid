ec9c690103cdb12e25b380e7c8494e6acac679f1
hoanghuydo787 <hoanghuydo787@gmail.com>
2024-01-24 16:08:10 +0700

    up

--- utils/misc.py
+++ utils/misc.py
@@ -89,7 +89,7 @@ def run_single(cfg, method, logger_save_dir):
         dirname=os.path.join(logger.log_dir, "auto_checkpoints"),
         filename_prefix="checkpoint",
         n_saved=3,
-        save_interval=5,
+        save_interval=2,
     )
 
     dm = init_dataset(
